1f83f2d66a737d7d486f4baeb2c0656f9a93e8ad
deepak <deepakkumardash313@gmail.com>
2025-08-06 17:57:57 +0530

    Add : wisget header and widget footer comp

--- apps/widget/modules/widget/ui/views/widget-view.tsx
+++ apps/widget/modules/widget/ui/views/widget-view.tsx
@@ -1,13 +1,18 @@
 "use client";
 
+import { WidgetFooter } from "../components/widget-footer";
+import { WidgetHeader } from "../components/widget-header";
+
 interface WidgetViewProps {
   organizationId: string;
 }
 
 export const WidgetView = ({ organizationId }: WidgetViewProps) => {
   return (
-    <main className="flex h-full w-full flex-col overflow-hidden rounded-xl border bg-muted">
-      Widget view : {organizationId}
+    <main className="flex h-full w-full flex-col overflow-hidden rounded-xl border bg-muted min-h-screen min-w-screen">
+      <WidgetHeader>Header</WidgetHeader>
+      <div className="flex flex-1">Widget view : {organizationId}</div>
+      <WidgetFooter />
     </main>
   );
 };
