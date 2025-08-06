185365198eaa4f8415994e04765bc0e8ed9f8dd3
deepak <deepakkumardash313@gmail.com>
2025-08-06 18:02:28 +0530

    Fix : Widget header content

--- apps/widget/modules/widget/ui/views/widget-view.tsx
+++ apps/widget/modules/widget/ui/views/widget-view.tsx
@@ -10,7 +10,12 @@ interface WidgetViewProps {
 export const WidgetView = ({ organizationId }: WidgetViewProps) => {
   return (
     <main className="flex h-full w-full flex-col overflow-hidden rounded-xl border bg-muted min-h-screen min-w-screen">
-      <WidgetHeader>Header</WidgetHeader>
+      <WidgetHeader>
+        <div className="flex flex-col justify-between gap-y-2 px-2 py-6 font-semibold">
+          <p className="text-3xl">Hi there! 👋🏻👋🏻</p>
+          <p className="text-lg">How can we help you today?</p>
+        </div>
+      </WidgetHeader>
       <div className="flex flex-1">Widget view : {organizationId}</div>
       <WidgetFooter />
     </main>
