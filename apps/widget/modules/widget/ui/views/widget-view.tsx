001f648e81eeee760924850d98554485969fe856
deepak <deepakkumardash313@gmail.com>
2025-08-08 17:29:26 +0530

    Add : jotai and setup

--- apps/widget/modules/widget/ui/views/widget-view.tsx
+++ apps/widget/modules/widget/ui/views/widget-view.tsx
@@ -1,17 +1,29 @@
 "use client";
 
-import { WidgetFooter } from "../components/widget-footer";
+import { useAtomValue } from "jotai";
 import { WidgetAuthScreen } from "../screens/widget-auth-screen";
+import { screenAtom } from "../../atoms/widget-atoms";
 
 interface WidgetViewProps {
   organizationId: string;
 }
 
 export const WidgetView = ({ organizationId }: WidgetViewProps) => {
+  const screen = useAtomValue(screenAtom);
+  const screenComponents = {
+    auth: <WidgetAuthScreen />,
+    error: <div>TODO: Error</div>,
+    loading: <div>TODO: Loading</div>,
+    voice: <div>TODO: Voice</div>,
+    inbox: <div>TODO: Inbox</div>,
+    selection: <div>TODO: Selection</div>,
+    chat: <div>TODO: Chat</div>,
+    contact: <div>TODO: Contact</div>,
+  };
+
   return (
     <main className="flex h-full w-full flex-col overflow-hidden rounded-xl border bg-muted min-h-screen min-w-screen">
-      <WidgetAuthScreen />
-      {/* <WidgetFooter /> */}
+      {screenComponents[screen]}
     </main>
   );
 };
