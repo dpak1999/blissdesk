4c74066ef86daa8595e8663d2594d66bbcc1d975
deepak <deepakkumardash313@gmail.com>
2025-08-24 18:30:56 +0530

    feat: enhance WidgetChatScreen with message handling and AI integration

--- apps/widget/modules/widget/ui/screens/widget-chat-screen.tsx
+++ apps/widget/modules/widget/ui/screens/widget-chat-screen.tsx
@@ -10,8 +10,33 @@ import {
   organizationIdAtom,
   screenAtom,
 } from "../../atoms/widget-atoms";
-import { useQuery } from "convex/react";
+import { useAction, useQuery } from "convex/react";
 import { api } from "@workspace/backend/_generated/api";
+import { toUIMessages, useThreadMessages } from "@convex-dev/agent/react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import {
+  AIConversation,
+  AIConversationContent,
+} from "@workspace/ui/components/ai/conversation";
+import {
+  AIMessage,
+  AIMessageContent,
+} from "@workspace/ui/components/ai/message";
+import { AIResponse } from "@workspace/ui/components/ai/response";
+import { Form, FormField } from "@workspace/ui/components/form";
+import {
+  AIInput,
+  AIInputSubmit,
+  AIInputTextarea,
+  AIInputToolbar,
+  AIInputTools,
+} from "@workspace/ui/components/ai/input";
+
+const formSchema = z.object({
+  message: z.string().min(1, "Please type a message"),
+});
 
 export const WidgetChatScreen = () => {
   const conversationId = useAtomValue(conversationIdAtom);
@@ -22,6 +47,11 @@ export const WidgetChatScreen = () => {
     contactSessionIdAtomFamily(organizationId || "")
   );
 
+  const onBack = () => {
+    setConversationId(null);
+    setScreen("selection");
+  };
+
   const conversation = useQuery(
     api.public.conversations.getOne,
     conversationId && contactSessionId
@@ -29,9 +59,37 @@ export const WidgetChatScreen = () => {
       : "skip"
   );
 
-  const onBack = () => {
-    setConversationId(null);
-    setScreen("selection");
+  const messages = useThreadMessages(
+    api.public.messages.getMany,
+    conversation?.threadId && contactSessionId
+      ? {
+          threadId: conversation.threadId,
+          contactSessionId,
+        }
+      : "skip",
+    { initialNumItems: 10 }
+  );
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      message: "",
+    },
+  });
+
+  const createMessage = useAction(api.public.messages.create);
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    if (!conversation || !contactSessionId) {
+      return;
+    }
+
+    form.reset();
+
+    await createMessage({
+      threadId: conversation.threadId,
+      prompt: values.message,
+      contactSessionId,
+    });
   };
 
   return (
@@ -49,9 +107,65 @@ export const WidgetChatScreen = () => {
         </Button>
       </WidgetHeader>
 
-      <div className="flex flex-1 flex-col gap-y-4 p-4">
-        {JSON.stringify(conversation, null, 4)}
-      </div>
+      <AIConversation>
+        <AIConversationContent>
+          {toUIMessages(messages.results ?? [])?.map((message) => {
+            return (
+              <AIMessage
+                from={message.role === "user" ? "user" : "assistant"}
+                key={message.id}
+              >
+                <AIMessageContent>
+                  <AIResponse color="red">{message.content}</AIResponse>
+                </AIMessageContent>
+                {/* TODO : add avatar */}
+              </AIMessage>
+            );
+          })}
+        </AIConversationContent>
+      </AIConversation>
+
+      {/* TODO : Add suggestions */}
+      <Form {...form}>
+        <AIInput
+          className="rounded-none border-x-0 border-b-0"
+          onSubmit={form.handleSubmit(onSubmit)}
+        >
+          <FormField
+            control={form.control}
+            disabled={conversation?.status === "resolved"}
+            name="message"
+            render={({ field }) => (
+              <AIInputTextarea
+                placeholder={
+                  conversation?.status === "resolved"
+                    ? "This conversation has been resolved"
+                    : "Please type your message..."
+                }
+                disabled={conversation?.status === "resolved"}
+                value={field.value}
+                onChange={field.onChange}
+                onKeyDown={(e) => {
+                  if (e.key === "Enter" && !e.shiftKey) {
+                    e.preventDefault();
+                    form.handleSubmit(onSubmit)();
+                  }
+                }}
+              />
+            )}
+          />
+          <AIInputToolbar>
+            <AIInputTools />
+            <AIInputSubmit
+              status="ready"
+              type="submit"
+              disabled={
+                conversation?.status === "resolved" || !form.formState.isValid
+              }
+            />
+          </AIInputToolbar>
+        </AIInput>
+      </Form>
     </>
   );
 };
--- packages/backend/convex/public/conversations.ts
+++ packages/backend/convex/public/conversations.ts
@@ -1,6 +1,8 @@
 import { ConvexError, v } from "convex/values";
 import { mutation, query } from "../_generated/server";
 import { supportAgent } from "../system/ai/agents/supportAgent";
+import { saveMessage } from "@convex-dev/agent";
+import { components } from "../_generated/api";
 
 export const getOne = query({
   args: {
@@ -55,6 +57,14 @@ export const create = mutation({
 
     const { threadId } = await supportAgent.createThread(ctx, {
       userId: args.organizationId,
+    });
+
+    await saveMessage(ctx, components.agent, {
+      threadId,
+      message: {
+        role: "assistant",
+        content: "Hello how can I help you today?",
+      },
     });
 
     const conversationId = await ctx.db.insert("conversations", {
